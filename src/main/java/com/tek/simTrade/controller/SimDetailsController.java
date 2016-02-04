5852229784ee4fc3ea9db41d0e1486a877364705
pratheeth <pratheeth@gmail.com>
2016-02-04 14:20:05 +0530

    initial commit to sim trade app

---
+++ src/main/java/com/tek/simTrade/controller/SimDetailsController.java
@@ -0,0 +1,25 @@
+package com.tek.simTrade.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tek.simTrade.service.SimDetailsService;
+
+@RestController
+@RequestMapping(value="/sim-details/")
+public class SimDetailsController
+{
+	@Autowired
+	private SimDetailsService simDetailsService;
+	
+	@RequestMapping(value = "/create-sim-trade", method = RequestMethod.GET)
+	@ResponseBody
+	String createSimTrade()
+	{
+		simDetailsService.createSimTradeTable();
+		return "true";
+	}
+}
